5f4f0282a8adaa5e7bda12fabbdf909b3e6de3bb
Yvon MENSAH <yvonmensah22@gmail.com>
2024-09-20 23:00:18 +0100

    JS HTML DOM - Chapters: html

---
+++ html/js/script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("p1").innerHTML = "New Text";
+
+const element = document.getElementById("id01");
+element.innerHTML = "New Heading";
+
+// ----------------------------------------------------------------
+
+document.getElementById("image").src = "img/img.jpg";
+
+// ----------------------------------------------------------------
+
+document.getElementById("demo").innerHTML = "Date : " + Date();
